59f148cb301cb090e8bf7b47d321a62357528318
solairerove <solairerove@gmail.com>
2026-04-09 21:32:40 +0700

    Add `load_app_data` function stub and `Result` type alias in data module

--- src/data/mod.rs
+++ src/data/mod.rs
@@ -1,1 +1,10 @@
 pub mod models;
+
+use models::*;
+use std::path::Path;
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
+
+pub fn load_app_data(data_dir: &Path) -> Result<AppData> {
+    todo!()
+}
